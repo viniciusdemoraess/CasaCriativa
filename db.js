9ba17e792e472ab5d242f5b4b86dfc2cffe014b5
viniciusdemoraess <demoraesvini@outlook.com>
2021-09-08 11:10:26 -0400

    Atts

---
+++ db.js
@@ -0,0 +1,61 @@
+const sqlite3 = require('sqlite3').verbose() /* verbose para comunicar algumas mensagens importantes para nós*/
+
+const db = new sqlite3.Database('./ws.db')
+
+db.serialize(function (){
+    //Criar a tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT, 
+            description TEXT,
+            link TEXT
+        );
+    `)
+
+    //Inserir dado na tabela
+    /*const query = `
+    INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+    ) VALUES (?, ?, ?, ?, ?);
+    `
+
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Cursos de Programação",
+        "Estudo",
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        "https://rocketseat.com.br/"
+    ]
+
+    db.run(query, values, function (err){
+        if(err) return console.error(err)
+
+        console.log(this)
+    })*/
+
+    //Deletar um dado da tabela
+   /* db.run(`DELETE FROM ideas WHERE id = ?`, [2], function (err){
+        if (err) return console.error(err)
+
+        console.log("DELETADO", this)
+    })*/
+
+
+    //Consultar dados na tabela
+   /* db.all(`SELECT * FROM ideas`, function (err, rows){
+        if (err) return console.error(err)
+
+        console.log(rows)
+    })*/
+
+   
+})
+
+module.exports = db
--- server.js
+++ server.js
@@ -2,42 +2,14 @@
 const express = require("express")
 const server = express()
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        url: "https://rocketset.com.br/"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        url: "https://rocketset.com.br/"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        url: "https://rocketset.com.br/"
-    },
-
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em família",
-        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
-        url: "https://rocketset.com.br/"
-    },
-]
+const db = require("./db")
 
 //configurar arquivos estaticos 
 server.use(express.static("public"))
+
+//habilitar uso do req.body
+
+server.use(express.urlencoded({ extended: true }))
 
 //configuração do nunjucks
 const nunjucks = require("nunjucks")
@@ -50,23 +22,71 @@ nunjucks.configure("views", {
 // e capturo o pedido do cliente para responder
 server.get("/", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+            console.log(err)
+             return res.send("Erro no Banco de Dados!")
+         }
 
-    let lastIdeas = []
-    for(let idea of reversedIdeas){
-        if(lastIdeas.length < 2){
-            lastIdeas.push(idea)
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for(let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
         }
-    }
 
-    return res.render("index.html", {ideas: lastIdeas})
+        return res.render("index.html", {ideas: lastIdeas})
+    })
+
 })
 
 server.get("/ideias", function (req, res) {
 
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) {
+           console.log(err)
+            return res.send("Erro no Banco de Dados!")
+        }
 
-    return res.render("ideias.html", {ideas: reversedIdeas})
+        const reversedIdeas = [...rows].reverse()
+
+        return res.render("ideias.html", {ideas: reversedIdeas})
+    })
+})
+
+
+server.post('/', function (req, res) {
+
+    //Inserir dado na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?, ?, ?, ?, ?);
+    `
+
+    const values = [
+        req.body.image, 
+        req.body.title,         
+        req.body.category,          
+        req.body.description,          
+        req.body.link 
+
+    ]
+
+    db.run(query, values, function (err){
+        if (err) {
+            console.log(err)
+             return res.send("Erro no Banco de Dados!")
+         }
+
+        return res.redirect("/ideias")
+    })
 })
 
 
